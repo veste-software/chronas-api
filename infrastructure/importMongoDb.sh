d8ab9d2cb52d1b37bb7fb777ba75e5bea65b21e8
Joachim Aumann <aumannjoachim@gmail.com>
2019-06-21 22:56:31 +0200

    added restore command

--- infrastructure/importMongoDb.sh
+++ infrastructure/importMongoDb.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-host=tempchronasapi.documents.azure.com
-user=tempchronasapi
-password=N2Rn45wvSdzaqhhYoXZjNYAColQ6A8LMFqOtfdtXzfSf1REuOAMFLtCdPwgsgwVYehkxEQ9mRjOp7wyHYjko5A==
 
-#mongorestore --host $host:10255 -u $user -p $password --ssl --sslAllowInvalidCertificates --numInsertionWorkers 1 --batchSize 24 --drop --db chronas-api chronas-api
+host=<>
+user=chronas-mongo
+password=<>
 
-#mongorestore --host $host:10255 -u $user -p $password --ssl --sslAllowInvalidCertificates --numInsertionWorkers 1 --batchSize 24 --drop --db chronas-api chronas-api
-mongoimport --host $host:10255 -u $user -p $password --ssl --sslAllowInvalidCertificates --jsonArray --db chronas-api --collection chronas-api --file chronas-api/areas.metadata.json --numInsertionWorkers 4 --batchSize 24
+ mongorestore --gzip --archive=test-1558418400.gz --host $host --port 10255 --ssl -u $user -p $password --drop --sslAllowInvalidCertificates
